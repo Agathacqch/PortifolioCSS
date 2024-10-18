99c7b9a5c94caaf7856455ce62a324bc599d56d9
Agatha <agathacqch@gmail.com>
2024-10-17 21:44:43 -0300

    fazendo o commit

---
+++ sobremim.css
@@ -0,0 +1,48 @@
+body{
+    background-color: rgb(184, 206, 249);
+    color: rgb(97, 49, 112);
+    font-family: Arial, Helvetica, sans-serif;
+}
+ul{
+    display: flex;
+    justify-content: space-between;
+    list-style-type: none; 
+    align-items: center;
+    padding: 16px;
+}
+a{
+    color: white;
+    text-decoration: none;
+}
+img{
+    width: 150px;
+    height: 150px;
+    border-radius: 10%;
+    border: 5px solid rgb(170, 83, 83) ;
+}
+ 
+ol{
+    color: black;
+}
+h1{
+    color: rgb(7, 6, 7);
+}
+
+h2{
+    color: blueviolet;
+}
+ 
+h3{
+    color: rgb(240, 167, 141);
+}
+
+h4{
+    color: rgb(159, 22, 22);
+}
+p{
+    color: black;
+}
+
+ul{
+    color: black;
+}
